1f601a47d74bec7ffd4165221611e64ba7cf1b55
Durgesh Nandan Birmiwal <Durgeshagrawal38@gmail.com>
2021-12-14 19:54:18 +0530

    user signup created

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,26 @@
 const User = require("../models/User");
-
 const router = require("express").Router();
-
+const bcrypt = require("bcrypt");
 // register
-router.get("/register", async (req, res) => {
-  const user = await new User({
-    username: "tony",
-    email: "tonydragster69@gmail.com",
-    password: "123456",
-  });
-  await user.save();
+router.post("/register", async (req, res) => {
+  try {
+    // generate salt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // create user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // add user to db
+    const user = await newUser.save();
+    res.status(200).json(user);
+    console.log("sent!!!!!!");
+  } catch (error) {
+    console.log("\nERR :\n", error);
+  }
   res.send("ok");
 });
 
